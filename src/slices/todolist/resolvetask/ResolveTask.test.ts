7c63272ad1a04da954e31f14d93a8fe6daac1806
Martin Dilger <martin@nebulit.de>
2026-08-22 17:28:17 +0200

    feat: Resolve Task

---
+++ src/slices/todolist/resolvetask/ResolveTask.test.ts
@@ -0,0 +1,36 @@
+import {DeciderSpecification} from '@event-driven-io/emmett';
+import {
+    ResolveTaskCommand,
+    ResolveTaskInitialState,
+    decide,
+    evolve,
+} from './ResolveTaskCommand';
+import {describe, it} from 'node:test';
+
+describe('Resolve Task Specification', () => {
+    const given = DeciderSpecification.for({
+        decide,
+        evolve,
+        initialState: ResolveTaskInitialState,
+    });
+
+    it('spec: Resolve Task - creates TaskResolved event on empty stream', () => {
+        const command: ResolveTaskCommand = {
+            type: 'ResolveTask',
+            data: {
+                id: 'task-1',
+            },
+            metadata: {},
+        };
+
+        given([])
+            .when(command)
+            .then([{
+                type: 'TaskResolved',
+                data: {
+                    id: 'task-1',
+                },
+                metadata: {},
+            }]);
+    });
+});
---
+++ src/slices/todolist/resolvetask/ResolveTaskCommand.ts
@@ -0,0 +1,61 @@
+import type {Command} from '@event-driven-io/emmett';
+import {CommandHandler} from '@event-driven-io/emmett';
+import {type TodoListEvents} from '../TodoListEvents';
+import {findEventstore} from '../../../common/loadPostgresEventstore';
+
+export type ResolveTaskCommand = Command<'ResolveTask', {
+    id: string;
+}, {
+    correlation_id?: string;
+    causation_id?: string;
+}>;
+
+export type ResolveTaskState = {};
+
+export const ResolveTaskInitialState = (): ResolveTaskState => ({});
+
+export const evolve = (
+    state: ResolveTaskState,
+    event: TodoListEvents,
+): ResolveTaskState => {
+    const {type} = event;
+
+    switch (type) {
+        default:
+            return state;
+    }
+};
+
+export const decide = (
+    command: ResolveTaskCommand,
+    state: ResolveTaskState,
+): TodoListEvents[] => {
+    return [{
+        type: 'TaskResolved',
+        data: {
+            id: command.data.id,
+        },
+        metadata: {
+            correlation_id: command.metadata?.correlation_id,
+            causation_id: command.metadata?.causation_id,
+        },
+    }];
+};
+
+const ResolveTaskCommandHandler = CommandHandler<ResolveTaskState, TodoListEvents>({
+    evolve,
+    initialState: ResolveTaskInitialState,
+});
+
+export const handleResolveTask = async (id: string, command: ResolveTaskCommand) => {
+    const eventStore = await findEventstore();
+    const result = await ResolveTaskCommandHandler(
+        eventStore,
+        id,
+        (state: ResolveTaskState) => decide(command, state),
+    );
+    return {
+        nextExpectedStreamVersion: result.nextExpectedStreamVersion,
+        lastEventGlobalPosition: result.lastEventGlobalPosition,
+    };
+};
